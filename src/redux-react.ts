9040602f0bd1be7af77ccf6d59ae5b543c9ef4af
吴学谦 <wuxueqian@ezbuy.com>
2018-01-03 15:45:21 +0800

    init

---
+++ src/redux-react.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function connect(mapStateToProps, mapDispatchToProps) {
+    // TODO
+
+    return function(component) {
+        // TODO
+    };
+}
+
+export class Provider extends React.Component {
+    // TODO
+}
---
+++ src/redux.ts
@@ -0,0 +1,30 @@
+export function createStore(reducer) {
+    // TODO
+
+    function getState() {
+        // TODO
+    }
+
+    function subscribe(listener) {
+        // TODO
+    }
+
+    function dispatch(action) {
+        // TODO
+    }
+
+    function combineReducers(reducers) {
+        // TODO
+    }
+
+    return {
+        getState,
+        subscribe,
+        dispatch,
+        combineReducers
+    };
+}
+
+export function applyMiddleware(...middlewares) {
+    // TODO
+}
