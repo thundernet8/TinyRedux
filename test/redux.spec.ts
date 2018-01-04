3ac503d352e58e47726ccda72aa4145ed0313cc1
吴学谦 <wuxueqian@ezbuy.com>
2018-01-04 11:04:05 +0800

    test up dispatch

--- test/redux.spec.ts
+++ test/redux.spec.ts
@@ -1,7 +1,7 @@
 import * as assert from "assert";
 import { createStore, combineReducers } from "../src/redux";
 import { todos, user } from "./helpers/reducers";
-// import { ADD_TODO, LOGIN } from "./helpers/actionTypes";
+import { ADD_TODO, LOGIN } from "./helpers/actionTypes";
 
 describe("Initial state", () => {
     it("Initial state of todos should be an empty array", () => {
@@ -19,9 +19,22 @@ describe("Combine reducers", () => {
                 user
             })
         );
-        const state = store.getState();
+        let state = store.getState();
 
         assert.deepEqual(state.todos, []);
         assert.deepEqual(state.user, {});
+
+        store.dispatch({
+            type: ADD_TODO,
+            payload: {
+                text: "TODO1"
+            }
+        });
+
+        state = store.getState();
+
+        assert(Array.isArray(state.todos));
+        assert(state.todos.length === 1);
+        assert(state.todos[0].text === "TODO1");
     });
 });
